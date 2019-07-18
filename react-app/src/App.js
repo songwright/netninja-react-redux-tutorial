ffd440c560bc282a25c233b532f3b73f6ca125e6
Ramon Sanchez <songwright66@gmail.com>
2019-07-18 10:10:06 -0700

    feat: Replace old component with new component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,19 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My first React app!</h1>
+      <p>Welcome! :)</p>
     </div>
   );
 }
