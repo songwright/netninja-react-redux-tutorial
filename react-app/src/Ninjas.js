7b91b458890609fa6dac95341e43650a5be2b314
Ramon Sanchez <songwright66@gmail.com>
2019-07-19 17:02:00 -0700

    feat: Add component file Ninjas.js

---
+++ react-app/src/Ninjas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Ninjas() {
+  return (
+    <div className="ninja">
+      <div>Name: Ryu</div>
+      <div>Age: 30</div>
+      <div>Belt: Black</div>
+    </div>
+  );
+}
+
+export default Ninjas;
