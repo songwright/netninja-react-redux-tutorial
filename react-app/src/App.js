73d09577d8c4565e0e632ec00a70e0a9bbd409aa
Ramon Sanchez <songwright66@gmail.com>
2019-07-19 17:03:47 -0700

    feat: Install Ninjas into App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Ninjas from './Ninjas';
 
 function App() {
   return (
     <div className="App">
       <h1>My first React app!</h1>
       <p>Welcome! :)</p>
+      <Ninjas />
     </div>
   );
 }
