44a53ef488729171eefe45b8f7a7e161d9a0c63e
Ramon Sanchez <songwright66@gmail.com>
2019-07-22 12:26:42 -0700

    feat: Add key to JSC div

--- react-app/src/Ninjas.js
+++ react-app/src/Ninjas.js
@@ -5,7 +5,7 @@ class Ninjas extends Component{
     const { ninjas } = this.props;
     const ninjaList = ninjas.map(ninja => {
       return (
-        <div className="ninja">
+        <div className="ninja" key={ninja.id}>
           <div>Name: { ninja.name }</div>
           <div>Age: { ninja.age }</div>
           <div>Belt: { ninja.belt }</div>
