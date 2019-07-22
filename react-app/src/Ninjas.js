7d5840a6cb1de5cc8d4065525188e5b02ab74b5f
Ramon Sanchez <songwright66@gmail.com>
2019-07-22 12:01:33 -0700

    feat: Add function to map array to JSX output

--- react-app/src/Ninjas.js
+++ react-app/src/Ninjas.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class Ninjas extends Component{
   render(){
     const { ninjas } = this.props;
-    return (
-      <div className="ninja">
-        <div>Name: { name }</div>
-        <div>Age: { age }</div>
-        <div>Belt: { belt }</div>
+    const ninjaList = ninjas.map(ninja => {
+      return (
+        <div className="ninja">
+          <div>Name: { ninja.name }</div>
+          <div>Age: { ninja.age }</div>
+          <div>Belt: { ninja.belt }</div>
       </div>
+      )
+    })
+    return (
+      
     );
   }
 }
