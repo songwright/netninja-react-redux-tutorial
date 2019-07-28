f171feb08232b626d7912b1ac59e2f68662c874a
Ramon Sanchez <songwright66@gmail.com>
2019-07-28 11:58:53 -0700

    feat: Replace if statement with ternary operator

--- react-app/src/Ninjas.js
+++ react-app/src/Ninjas.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
 const Ninjas = ({ninjas}) => {
+  // const ninjaList = ninjas.map(ninja => {
+  //   if (ninja.age > 20) {
+  //     return (
+  //       <div className="ninja" key={ninja.id}>
+  //         <div>Name: { ninja.name }</div>
+  //         <div>Age: { ninja.age }</div>
+  //         <div>Belt: { ninja.belt }</div>
+  //       </div>
+  //     )
+  //   } else {
+  //     return null;
+  //   }
+  // })
   const ninjaList = ninjas.map(ninja => {
-    if (ninja.age > 20) {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>Name: { ninja.name }</div>
-          <div>Age: { ninja.age }</div>
-          <div>Belt: { ninja.belt }</div>
+    return ninja.age > 20 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: { ninja.name }</div>
+        <div>Age: { ninja.age }</div>
+        <div>Belt: { ninja.belt }</div>
       </div>
-      )
-    } else {
-      return null;
-    }
-  })
+    ) : null;
+  });
   return (
     <div className="ninja-list">
       { ninjaList }
@@ -21,4 +30,4 @@ const Ninjas = ({ninjas}) => {
   );
 }
 
-export default Ninjas;
+export default Ninjas
