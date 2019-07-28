dc9c81b3102411c00b016d0476787a3cf4423abd
Ramon Sanchez <songwright66@gmail.com>
2019-07-28 12:10:26 -0700

    feat: Embed the JSX in the return statement

--- react-app/src/Ninjas.js
+++ react-app/src/Ninjas.js
@@ -14,20 +14,22 @@ const Ninjas = ({ninjas}) => {
   //     return null;
   //   }
   // })
-  const ninjaList = ninjas.map(ninja => {
-    return ninja.age > 20 ? (
-      <div className="ninja" key={ninja.id}>
-        <div>Name: { ninja.name }</div>
-        <div>Age: { ninja.age }</div>
-        <div>Belt: { ninja.belt }</div>
-      </div>
-    ) : null;
-  });
+  
   return (
     <div className="ninja-list">
-      { ninjaList }
+      {
+        ninjas.map(ninja => {
+          return ninja.age > 20 ? (
+            <div className="ninja" key={ninja.id}>
+              <div>Name: { ninja.name }</div>
+              <div>Age: { ninja.age }</div>
+              <div>Belt: { ninja.belt }</div>
+            </div>
+          ) : null;
+        })
+      }
     </div>
-  );
+  )
 }
 
 export default Ninjas
