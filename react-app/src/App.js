d805f1d52e546c67fd48ef9758712c0592953797
Ramon Sanchez <songwright66@gmail.com>
2019-07-30 10:48:27 -0700

    feat: Create addNinja function in App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,13 +10,18 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3}
     ]
   }
+
+addNinja = (ninja) => {
+  //
+}
+
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome! :)</p>
         <Ninjas ninjas={this.state.ninjas} />
-        <AddNinja />
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
