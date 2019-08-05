fcdeac808085e6b9f18aa301eef70846df3df28a
Ramon Sanchez <songwright66@gmail.com>
2019-08-05 16:39:36 -0700

    feat: Add state to App.js

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    todo: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario kart'}
+    ]
+  }
   render () {
     return (
       <div className="App">
