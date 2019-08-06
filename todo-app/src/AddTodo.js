93931586a6c0b0ee78379b0f9b81353e9cc71491
Ramon Sanchez <songwright66@gmail.com>
2019-08-06 10:50:23 -0700

    feat: Update handleSubmit in AddTodo.js

--- todo-app/src/AddTodo.js
+++ todo-app/src/AddTodo.js
@@ -12,13 +12,16 @@ class AddTodo extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addTodo(this.state);
+        this.setState({
+            content: ''
+        })
     }
     render(){
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>Add new todo:</label>
-                    <input type="text" onChange={this.handleChange} />
+                    <input type="text" onChange={this.handleChange} value={this.state.content} />
                 </form>
             </div>
         )
