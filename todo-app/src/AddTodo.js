909da928ec04d7884728a58eab219ca6ef45217c
Ramon Sanchez <songwright66@gmail.com>
2019-08-06 10:29:17 -0700

    feat: Update handleSubmit in AddTodo.js

--- todo-app/src/AddTodo.js
+++ todo-app/src/AddTodo.js
@@ -11,7 +11,7 @@ class AddTodo extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        this.props.addTodo(this.state);
     }
     render(){
         return (
