1cbb1ffadcb67feadc1d3fd75e7eced0c3ff9c80
Ramon Sanchez <songwright66@gmail.com>
2019-08-05 21:58:41 -0700

    feat: Add deleteTodo to App.js

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -8,11 +8,14 @@ class App extends Component {
       {id: 2, content: 'play mario kart'}
     ]
   }
+  deleteTodo = (id) => {
+    console.log(id);
+  }
   render () {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
