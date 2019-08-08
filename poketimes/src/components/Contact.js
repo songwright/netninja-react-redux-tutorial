28ea6d610abfe119f7b32eb077f3bd3e1d6098c1
Ramon Sanchez <songwright66@gmail.com>
2019-08-07 22:07:12 -0700

    Add Contact.js file

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="container">
+        <h4 className="center">Contact</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vitae maxime. Animi vitae, adipisci magnam nisi recusandae ut suscipit explicabo quos molestiae, ullam ratione ad unde, nesciunt optio illo nulla.</p>
+    </div>
+  )
+}
+
+export default Contact
