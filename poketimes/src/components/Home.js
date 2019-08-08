6f4ef5bcaa2b6b243a48413ff066e5758221370a
Ramon Sanchez <songwright66@gmail.com>
2019-08-07 22:04:24 -0700

    Create Home.js component file

---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container">
+        <h4 className="center">Home</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vitae maxime. Animi vitae, adipisci magnam nisi recusandae ut suscipit explicabo quos molestiae, ullam ratione ad unde, nesciunt optio illo nulla.</p>
+    </div>
+  )
+}
+
+export default Home
