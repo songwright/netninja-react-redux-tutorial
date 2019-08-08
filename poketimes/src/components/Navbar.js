1ffa292ad2b0e01be65ddfed869def0a27d40b87
Ramon Sanchez <songwright66@gmail.com>
2019-08-07 22:38:03 -0700

    fix: Move text in links to correct positions

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
       <div className="container">
         <a  className="brand-logo">Poke' Times</a>
         <ul className="right">
-          <li><a href="/"></a>Home</li>
-          <li><a href="/about"></a>About</li>
-          <li><a href="/contact"></a>Contact</li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
       </div>
     </nav>
