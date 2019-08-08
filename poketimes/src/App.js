c48034f3fcf84863101d24ee7a788e0ad6ce372f
Ramon Sanchez <songwright66@gmail.com>
2019-08-07 22:21:08 -0700

    feat: Insert Navbar references into App.js

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Navbar />
       </div>
     );
   }
