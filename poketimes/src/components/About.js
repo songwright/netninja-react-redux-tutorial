8dbd1bdb94a84097d92af7a0bab696aa9fe0c5f2
Ramon Sanchez <songwright66@gmail.com>
2019-08-07 22:06:50 -0700

    Add About.js file

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="container">
+        <h4 className="center">About</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vitae maxime. Animi vitae, adipisci magnam nisi recusandae ut suscipit explicabo quos molestiae, ullam ratione ad unde, nesciunt optio illo nulla.</p>
+    </div>
+  )
+}
+
+export default About
