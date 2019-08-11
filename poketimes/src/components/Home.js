04dd0347b556bc9dc0e3951ec18cdd1e8bfab4ce
Ramon Sanchez <songwright66@gmail.com>
2019-08-10 19:26:45 -0700

    feat: Add componentDidMount function in Home.js

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class Home extends Component {
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        console.log(res);
+      })
+  }
   render() {
       return (
       <div className="container">
