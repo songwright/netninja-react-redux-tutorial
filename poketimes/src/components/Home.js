ff6d586591659e6a223321fc5b9dc3332b4979fb
Ramon Sanchez <songwright66@gmail.com>
2019-08-10 19:12:56 -0700

    feat: Convert Home.js file into class-based component

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const Home = () => {
-  return (
-    <div className="container">
-        <h4 className="center">Home</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vitae maxime. Animi vitae, adipisci magnam nisi recusandae ut suscipit explicabo quos molestiae, ullam ratione ad unde, nesciunt optio illo nulla.</p>
-    </div>
-  )
+class Home extends Component {
+  render() {
+      return (
+      <div className="container">
+          <h4 className="center">Home</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vitae maxime. Animi vitae, adipisci magnam nisi recusandae ut suscipit explicabo quos molestiae, ullam ratione ad unde, nesciunt optio illo nulla.</p>
+      </div>
+    )
+  } 
 }
 
 export default Home
