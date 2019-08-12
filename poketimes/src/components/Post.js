2008446f2aadff602a8016999053a97c1d6e5462
Ramon Sanchez <songwright66@gmail.com>
2019-08-12 13:58:20 -0700

    feat: Add JSX block to render function in Post.js

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -15,9 +15,19 @@ class Post extends Component{
     })
   }
   render() {
+
+    const post = this.state.post ? (
+      <div className="post">
+        <h4 className="center">{this.state.post.title}</h4>
+        <p>{this.state.post.body}</p>
+      </div>
+    ) : (
+      <div className="center">Loading post ...</div>
+    )
+
     return (
       <div className="container">
-        <h4></h4>
+        { post }
       </div>
     )
   }
