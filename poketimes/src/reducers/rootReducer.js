ae7ff51c57265f0e788b55472d30bdf5acafae04
Ramon Sanchez <songwright66@gmail.com>
2019-08-25 13:19:43 -0700

    feat: Add dummy data to rootReducer state

--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-  posts: []
+  posts: [
+    {id: '1', title: 'Squirtle Laid an Egg', body: 'Aliqua enim et excepteur nisi velit occaecat aliquip qui non.'},
+    {id: '2', title: 'Charmander Laid an Egg', body: 'Aliqua enim et excepteur nisi velit occaecat aliquip qui non.'},
+    {id: '3', title: 'A Helix Fossil was Found', body: 'Aliqua enim et excepteur nisi velit occaecat aliquip qui non.'}
+  ]
 }
 
 const rootReducer = (state = initState, action) => {
@@ -7,3 +11,4 @@ const rootReducer = (state = initState, action) => {
 }
 
 export default rootReducer
+
