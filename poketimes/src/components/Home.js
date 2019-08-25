1146301ba32479d98ea67b903f8f941646acd5eb
Ramon Sanchez <songwright66@gmail.com>
2019-08-25 13:38:53 -0700

    feat: Remove state and componentDidMount from Home.js

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,21 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
 
 class Home extends Component {
-  state = {
-    posts: [ ]
-  }
-  componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => {
-        console.log(res);
-        this.setState({
-          posts: res.data.slice(0,10)
-        })
-      })
-  }
   render() {
     const { posts } = this.state;
     const postList = posts.length ? (
