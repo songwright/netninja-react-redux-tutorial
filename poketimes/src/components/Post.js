c8060111dc7e93a9724d5b9f1d5ce76916459653
Ramon Sanchez <songwright66@gmail.com>
2019-08-26 12:19:38 -0700

    feat: Add mapStateToProps function in Post.js

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -21,4 +21,13 @@ class Post extends Component{
   }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find((post) => {
+      post.id === id
+    })
+  }
+}
+
 export default connect(mapStateToProps) (Post)
