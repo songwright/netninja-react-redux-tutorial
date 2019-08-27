956fdfd838f72bce0a50f628ec9bcc4fbfc9b34c
Ramon Sanchez <songwright66@gmail.com>
2019-08-27 11:52:42 -0700

    feat: Add Delete Post button to Post.js

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -8,6 +8,11 @@ class Post extends Component{
       <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading post ...</div>
